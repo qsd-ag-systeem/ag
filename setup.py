0c06f107bc4704e2c755286c7d0abbd94f296f50
Ward Pieters <ward@wardpieters.nl>
2022-11-30 15:27:49 +0100

    typo

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-    name='Automatisch Gelaatsherkennings Systeem',
+    name='Automatisch Gelaat Herkenningssysteem',
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
