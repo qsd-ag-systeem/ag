317b7f1cabe75094eed85022162d8641f9f1eb9c
Joël Kuijper <joel@deindruk.nl>
2022-11-30 14:18:58 +0100

    feat

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Automatisch Gelaatsherkennings Systeem
 
 [Convential Commits](https://www.conventionalcommits.org/en/v1.0.0/)
+
+# Installation
+
+```bash
+pip install -r requirement.txt
+pip install --editable .
+```
--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from setuptools import setup
-
+from setuptools import setup, find_packages
 setup(
     name='Automatisch Gelaatsherkennings Systeem',
     version='0.1.0',
-    py_modules=['cli/ag.py'],
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'Click',
     ],
     entry_points={
         'console_scripts': [
-            'cli = ag:cli',
+            'ag = cli.ag:cli',
         ],
     },
 )
