3dbbd45666401e418fbde81afed3e031e190df3b
willemsteller1 <82500231+willemsteller1@users.noreply.github.com>
2022-12-06 12:02:46 +0100

    test: process_file unsupported file

--- core/__test__/face_recognition_test.py
+++ core/__test__/face_recognition_test.py
@@ -1,5 +1,8 @@
 from core.face_recognition import insert_data, init, vec2list, process_file
 from dlib import vector
+from click.testing import CliRunner
+import os
+from pathlib import Path
 
 def test_insert_data():
     dataset = "test"
@@ -21,3 +24,19 @@ def test_vec2list():
     vec = vector([1, 0])
     res = vec2list(vec)
     assert res == [1, 0]
+
+def test_process_file_not_supported():
+    runner = CliRunner()
+
+    with runner.isolated_filesystem():
+        os.mkdir('exists')
+        with open('exists/test.txt', 'w') as f:
+            f.write('test')
+
+            file = Path('exists/test.txt')
+            try:
+                res = process_file("test", file)
+                assert False
+            except Exception as e:
+                assert True
+                assert "File not supported" in str(e)
