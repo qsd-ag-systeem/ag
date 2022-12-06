bec17ac97090152ef1fb0e145670304a13ba656c
willemsteller1 <82500231+willemsteller1@users.noreply.github.com>
2022-12-06 11:23:06 +0100

    test: face_recognition (wip)

---
+++ core/__test__/face_recognition_test.py
@@ -0,0 +1,23 @@
+from core.face_recognition import insert_data, init, vec2list, process_file
+from dlib import vector
+
+def test_insert_data():
+    dataset = "test"
+    file_name = "test.jpg"
+    face_emb = vec2list([1, 2, 3])
+    width = 100
+    height = 100
+    x = (0, 0)
+    y = (0, 0)
+    insert_data(dataset, file_name, face_emb, width, height, x, y)
+
+    assert True
+
+def test_init():
+    res = init(False)
+    assert res is None
+
+def test_vec2list():
+    vec = vector([1, 0])
+    res = vec2list(vec)
+    assert res == [1, 0]
