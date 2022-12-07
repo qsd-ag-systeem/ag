ccf49ec264a6f454207d5de8eaff5f75a714a449
Ward Pieters <ward@wardpieters.nl>
2022-12-07 16:41:50 +0100

    refactor: improve vec_sub function

--- core/sql/euclidian_func.sql
+++ core/sql/euclidian_func.sql
@@ -4,10 +4,8 @@ CREATE OR REPLACE FUNCTION public.vec_sub(arr1 double precision[], arr2 double p
  STRICT
 AS $function$
 SELECT array_agg (result)
-    FROM (SELECT (tuple.val1 - tuple.val2) * (tuple.val1 - tuple.val2)
+    FROM (SELECT (val1 - val2) * (val1 - val2)
         AS result
         FROM (SELECT UNNEST ($1) AS val1
-               , UNNEST ($2) AS val2
-               , generate_subscripts ($1, 1) AS ix) tuple
-    ORDER BY ix) inn;
+               , UNNEST ($2) AS val2) tuple) inn;
 	$function$
