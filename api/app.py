9c6cc4e1eb93573fb5f703f1224dce6e3c02011a
willemsteller1 <82500231+willemsteller1@users.noreply.github.com>
2022-12-13 13:17:21 +0100

    feat api command

--- api/app.py
+++ api/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,3 +7,18 @@ def hello_world():
     return {
         "message": "Hello, World!"
     }
+
+@app.route('/enroll', methods=['POST'])
+def enroll():
+    return {
+        "message": "Enroll"
+    }
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return {
+        "message": "Page not found"
+    }, 404
+
+def run():
+    app.run(host='0.0.0.0', port=5000, debug=True)
