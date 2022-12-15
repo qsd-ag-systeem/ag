fad960348e16582b27c01ace98487c11f245bf98
Kabelbaan <bosjoost201@gmail.com>
2022-12-15 11:54:54 +0100

    refactor: api and commands

---
+++ cli/commands/enroll.py
@@ -0,0 +1,45 @@
+import click
+import os
+from core.common import get_files
+from core.face_recognition import init, process_file, use_cuda
+
+
+@click.command()
+@click.argument('folder', type=click.Path(exists=True))
+@click.option('--debug/--no-debug', default=False)
+@click.option('--cuda/--no-cuda', default=True)
+def enroll(folder: str, debug: bool, cuda: bool) -> None:
+    """
+    Enroll the images from the given folder into the database.
+    """
+    folder_path = os.path.abspath(os.path.join(os.getcwd(), folder))
+    files = get_files(folder_path)
+
+    cuda = use_cuda(cuda)
+    init(cuda)
+
+    errors = []
+
+    click.echo("⚡ Using CUDA!" if cuda else "🐢 CUDA not available, falling back to CPU processing!")
+
+    if len(files) == 0:
+        click.echo(f"Folder {folder} is empty!")
+        return
+
+    with click.progressbar(files, show_pos=True, show_percent=True, label="Initializing...") as bar:
+        for file in bar:
+            try:
+                process_file(folder, file, cuda)
+                bar.label = f"Processing: {os.path.relpath(file)}"
+            except Exception as error:
+                bar.label = f"Error processing: {os.path.relpath(file)}"
+
+                if debug:
+                    errors.append(error)
+
+                pass
+
+    for error in errors:
+        click.echo(error)
+
+    click.echo('Enrollment finished!')
