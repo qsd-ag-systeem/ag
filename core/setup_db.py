fe5b6dc84fb193245c0021cd3e4443a6dac7f5ce
Joël Kuijper <jmkuijper@outlook.com>
2022-12-20 13:40:46 +0100

    fix: sql functions

--- core/setup_db.py
+++ core/setup_db.py
@@ -2,14 +2,16 @@ import os
 from core.db import Session, engine, Base
 from core.model.face import Face
 
+
 def setup_db():
     root_dir = os.path.abspath(os.curdir)
 
     Base.metadata.create_all(engine)
     session = Session()
-    
-    session.execute(open(root_dir + "/core/sql/euclidian_func.sql", "r").read())
+
     session.execute(open(root_dir + "/core/sql/vec_sub_func.sql", "r").read())
+    session.execute(
+        open(root_dir + "/core/sql/euclidian_func.sql", "r").read())
 
     session.commit()
     session.close()
