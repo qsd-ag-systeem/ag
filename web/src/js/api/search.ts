6d9b897eb306f7d11c7de3aa8d8e6a661097b610
Kabelbaan <bosjoost201@gmail.com>
2023-01-09 11:07:27 +0100

    feat: enroll / search

---
+++ web/src/js/api/search.ts
@@ -0,0 +1,21 @@
+import { API_URL, FETCH_HEADERS } from "../constants";
+
+type BodySearch = {
+    folder: string,
+    cuda?: boolean,
+    dataset?: string
+}
+
+export const fetchSearch = async (data: BodySearch) => {
+    return await fetch(`${API_URL}/search`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        ...FETCH_HEADERS()
+    })
+        .then((data) => {
+            return data.json();
+        })
+        .catch((error) => {
+            throw error.message;
+        });
+};
