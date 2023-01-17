95af7c7a94f4beb5554fb80cb9271f4d4f32e48c
Joël Kuijper <jmkuijper@outlook.com>
2023-01-17 09:58:24 +0100

    chore: fix ag.py

--- cli/ag.py
+++ cli/ag.py
@@ -4,15 +4,15 @@ CLI for the face recognition application.
 import click
 from halo import Halo
 
-from core.setup_db import ensure_index_exists, ensure_db_running
-from cli.commands.enroll import enroll
-from cli.commands.search import search
-from cli.commands.get_datasets import get_datasets
-from cli.commands.delete_dataset import delete_dataset
-from cli.commands.export_dataset import export_dataset
-from cli.commands.import_dataset import import_dataset
-from cli.commands.cross_search import cross_search
 from api.app import run as run_api
+from cli.commands.cross_search import cross_search
+from cli.commands.delete_dataset import delete_dataset
+from cli.commands.enroll import enroll
+from cli.commands.export_dataset import export_dataset
+from cli.commands.get_datasets import get_datasets
+from cli.commands.import_dataset import import_dataset
+from cli.commands.search import search
+from core.setup_db import ensure_db_running, ensure_index_exists
 
 
 @click.group(help="Dit is de command line interface (CLI) voor het gebruiken van het automatisch gelaat herkenningssysteem")
@@ -23,18 +23,12 @@ def cli():
     init_app()
 
 
-@click.option('--debug/--no-debug', default=False)
 @cli.command(help="Start de web applicatie")
-<<<<<<< HEAD
-def web(debug: bool):
-    run_api('127.0.0.1', 8080, debug)
-=======
 @click.option('--host', type=str, default='127.0.0.1', help='Host waarop de web applicatie draait')
 @click.option('--port', type=int, default=8080, help='Poort waarop de web applicatie draait')
 @click.option('--debug/--no-debug', default=False, help='Debug mode')
 def web(host: str, port: int, debug: bool):
     run_api(host, port, debug)
->>>>>>> 62f0fbe96141f2a501bf9c0f56aeacfbbfda8614
 
 
 @cli.command(hidden=True)
