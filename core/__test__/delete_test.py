8cf5e95869b95327a636edee13ecb4f18587102e
LukaUDBoogaardt <s1154960@student.windesheim.nl>
2023-01-17 13:23:25 +0100

    feat: delete tests

---
+++ core/__test__/delete_test.py
@@ -0,0 +1,48 @@
+from unittest.mock import MagicMock, patch
+from unittest import TestCase
+
+from elasticsearch import NotFoundError
+from core.delete import delete_dataset_by_name, delete_dataset_file_by_name, delete_dataset_files_by_name, delete_file_by_name
+
+class TestDeleteFile(TestCase):
+    dataset = "dataset"
+    file_name = "image.jpg"
+
+    @patch('core.delete.dataset_exists')
+    def test_delete_file_by_name_dataset_not_exists(self, mock_dataset_exists):
+        mock_dataset_exists.return_value = False
+
+        with self.assertRaises(Exception) as context:
+            delete_file_by_name(self.dataset, self.file_name)
+
+        self.assertIn("Dataset '{}' does not exist.".format(self.dataset), str(context.exception))
+        
+
+    @patch('core.delete.dataset_exists')
+    @patch('core.delete.EsConnection')
+    def test_delete_by_query_called_with_correct_args(self, mock_es_conn, mock_dataset_exists):
+        mock_dataset_exists.return_value = True
+        mock_es_conn.return_value = mock_es_conn
+        mock_es_conn.index_name = "face_embeddings"
+
+        dataset = 'dataset1'
+        file_name = 'image1.jpg'
+        delete_file_by_name(dataset, file_name)
+
+        print(mock_es_conn.connection.delete_by_query.call_args_list)
+        mock_es_conn.connection.delete_by_query.assert_called_once_with(index='face_embeddings', query={
+            "bool": {
+                "must": [
+                    {
+                        "term": {
+                            "dataset": dataset,
+                        },
+                    },
+                    {
+                        "term": {
+                            "file_name": file_name,
+                        },
+                    }
+                ]
+            }
+        })
